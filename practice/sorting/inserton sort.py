cc6ac3bf7c6154afb07afda7d5897af059bd5780
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-11-25 02:53:23 +0530

    minor fix

--- practice/sorting/inserton sort.py
+++ practice/sorting/inserton sort.py
@@ -2,15 +2,15 @@ def insertion_sort(arr):
     n = len(arr)
 
     for i in range(1, n):
-        Value = arr[i]
+        key = arr[i]
         j = i - 1
 
         # Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
-        while j >= 0 and Value < arr[j]:
+        while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
 
-        arr[j + 1] = Value
+        arr[j + 1] = key
 
 # Example usage:
 my_array = [64, 25, 12, 22, 11]
