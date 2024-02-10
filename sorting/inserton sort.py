9280c47005ab84ba1c447735e46bb29a3dc44c58
Prikshit7766 <prikshitsharma8024@gmail.com>
2024-02-10 20:29:08 +0530

    updated folder structure

--- sorting/inserton sort.py
+++ sorting/inserton sort.py
@@ -1,11 +1,16 @@
 def insertion_sort(arr):
     n = len(arr)
 
+    # Best case: If the array is already sorted, the inner loop won't need to perform any shifts.
+    # In this case, the time complexity is O(n), as the outer loop iterates once through the array.
+    
+    # Worst case: If the array is sorted in reverse order, each element will need to be compared and shifted to its correct position.
+    # In this case, the time complexity is O(n^2), as each element may require up to 'i' comparisons and shifts,
+    # where 'i' is the index of the outer loop.
     for i in range(1, n):
         key = arr[i]
         j = i - 1
 
-        # Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
         while j >= 0 and key < arr[j]:
             arr[j + 1] = arr[j]
             j -= 1
